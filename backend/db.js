67a657e58d8c9665c255f53961faabecbc1ad046
flandercanaj <flandercanaj18@gmail.com>
2025-05-19 07:56:29 +0200

    Add docker-compose with api, db, nginx; add MongoDB persistent volume; configure nginx proxy

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const dbUrl = process.env.DB_URL || "mongodb://localhost/vidly";
+const dbUrl = process.env.DB_URL || "mongodb://localhost/db_data";
 
 const connect = async () => {
   await mongoose.connect(dbUrl, {
